e67971d3a65173d4786d1c14154c66b6964e42db
Ley Makara <ley.makara00@gmail.com>
2024-04-04 14:55:55 +0700

    add hover effect on fill box

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -6,20 +6,20 @@ const Box = () => {
     <div className='text-primary font-geologica font-bold flex flex-col gap-2 justify-center items-center'>
       <h2>Fill:</h2>
       <div className="flex justify-center items-center gap-1">
-        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center'>
-          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'>A</h1>
+        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center hover:border-white hover:bg-secondary/25 transition duration-100'>
+          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'></h1>
         </div>
-        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center'>
-          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'>P</h1>
+        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center hover:border-white hover:bg-secondary/25 transition duration-100'>
+          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'></h1>
         </div>
-        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center'>
-          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'>R</h1>
+        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center hover:border-white hover:bg-secondary/25 transition duration-100'>
+          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'></h1>
         </div>
-        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center'>
-          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'>I</h1>
+        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center hover:border-white hover:bg-secondary/25 transition duration-100'>
+          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'></h1>
         </div>
-        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center'>
-          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'>L</h1>
+        <div className='flex w-[50px] h-[50px] border-solid border-black/70 border-[1px] bg-secondary rounded-md justify-center items-center hover:border-white hover:bg-secondary/25 transition duration-100'>
+          <h1 className='bg-transparent text-black font-geologica text-[26px] font-bold'></h1>
         </div>
       </div>
     </div>
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,29 +1,46 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 
 const Keyboard = () => {
   return (
     /* A whole Keyboard layout */
-    <div className='font-geologica font-bold'>
+    <div className='font-geologica font-bold w-full px-3 flex flex-col gap-1 items-center'>
       {/* First Row */}
-      <div className='flex flex-row justify-evenly w-full text-center'>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
-        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px]'>A</button>
+      <div className='flex flex-row justify-evenly w-[390px] text-center gap-1'>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>Q</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>W</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>E</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>R</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>T</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>Y</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>U</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>I</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>O</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>P</button>
       </div>
       {/* Second Row */}
-      <div>
-
+      <div className='flex flex-row justify-evenly w-[350px] text-center gap-1'>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>A</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>S</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>D</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>F</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>G</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>H</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>J</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>K</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>L</button>
       </div>
       {/* Third Row */}
-      <div>
-
+      <div className='flex flex-row justify-evenly w-full text-center gap-1'>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>ENTER</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>Z</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>X</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>C</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>V</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>B</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>N</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>M</button>
+        <button className='border-solid border-black/70 border-[2px] bg-secondary rounded-md items-center flex cursor-pointer min-h-[46px] justify-center p-[3px] w-full'>Backspace</button>
       </div>
     </div>
   )
